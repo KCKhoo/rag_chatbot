e3b84a787471b25b88401d76f874f5c9658c4d90
Khoo Kian Chong <kckhoo@Khoos-MacBook-Air.local>
2026-02-15 21:39:59 +0800

    Add rag pipeline

---
+++ src/module/rag_pipeline.py
@@ -0,0 +1,44 @@
+from src.module.retrieval_pipeline import RetrievalPipeline
+from src.module.response_generation_pipeline import ResponseGenerationPipeline
+
+
+class RAGPipeline:
+    def __init__(
+        self,
+        retrieval_pipeline: RetrievalPipeline,
+        response_generation_pipeline: ResponseGenerationPipeline,
+    ):
+        """
+        Create an end-to-end RAG pipeline
+
+        :param retrieval_pipeline: An instance of RetrievalPipeline
+        :param generation_pipeline: An instance of ResponseGenerationPipeline
+        """
+        self.retrieval_pipeline = retrieval_pipeline
+        self.response_generation_pipeline = response_generation_pipeline
+
+    def answer(self, question: str) -> str:
+        """Answer user question"""
+
+        try:
+            retrieved_context = self.retrieval_pipeline.retrieve_contexts(question)
+            answer = self.response_generation_pipeline.generate_response(
+                question, retrieved_context
+            )
+
+            return {
+                "status": "success",
+                "error_message": None,
+                "question": question,
+                "answer": answer,
+                "context": retrieved_context,
+                # Future Improvement: Output latency, token usage for model monitoring
+            }
+        except Exception as e:
+            return {
+                "status": "error",
+                "error_message": str(e),
+                "question": question,
+                "answer": None,
+                "context": None,
+            }
